1fbe10029a4e078d7ab972cf664d8c956ff9e0bc
roz-pnv <rozhinpanaw@gmail.com>
2024-08-31 01:03:06 +0330

    update book model
    makemigration in book app

--- library/book/models.py
+++ library/book/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Book(models.Model):
+    class Category(models.TextChoices):
+        Biography = 'B', 'Biography'
+        Childrens_fiction = 'C', 'Childrens fiction'
+        Fantasy  = 'F', 'Fantasy'
+        Horror = 'H', 'Horror'
+        Inspirational  = 'I', 'Inspirational'
+        Mystery = 'M', 'Mystery'
+        Romance = 'R', 'Romance'
+        Young_adult = 'V', 'Young adult'
+
+
+    #Data field
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+
+
+    #Date field
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    #Choice field
+    category = models.CharField(
+        max_length=2, 
+        choices=Category.choices, 
+        default=Category.Biography
+    )
+
+    class  Meta:
+        ordering = [
+            '-publish',
+        ]
+        indexes = [
+            models.Index(fields=['-publish',]),
+        ]
+
+
+    def __str__ (self):
+        return self.name
